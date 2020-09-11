7f39950c04baab6d7a7444b9134963e8b71b3929
Tristram Gräbener <tristram@midnighttrains.com>
2020-09-11 18:08:05 +0200

    Make it responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -118,7 +118,7 @@ export default function AllCounters({ counts, metadata }: Props) {
           sous licence MIT
         </p>
       </div>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {_.map(stats, (stat) => (
           <Counter key={stat.id} stat={stat} />
         ))}
