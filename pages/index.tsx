27936be558c9a247adebf6c91db866697cea7519
Tristram Gräbener <tristram@midnighttrains.com>
2020-09-15 15:59:01 +0200

    Separate counter tile in a separate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,44 +28,6 @@ type Props = {
   }
 }
 
-const Num = ({ n }: { n: number }) => (
-  <span className="font-mono">{n.toLocaleString('fr-FR')}</span>
-)
-
-function Counter({ stat }: { stat: CounterStat }) {
-  return (
-    <>
-      <h2>{stat.label}</h2>
-      <dl className="pt-2">
-        <dt>Hier</dt>
-        <dd>
-          <Num n={stat.yesterday} />
-        </dd>
-        <dt>Sur 7 jours </dt>
-        <dd>
-          <Num n={stat.lastWeek} />
-        </dd>
-        <dt>Sur 30 jours </dt>
-        <dd>
-          <Num n={stat.last30Days} />
-        </dd>
-        <dt>Moyenne</dt>
-        <dd>
-          (sur {stat.days} jours) <Num n={stat.average} />
-        </dd>
-        <dt>Compteurs</dt>
-        <dd>
-          <ul className="text-xs">
-            {stat.included.map((counter) => (
-              <li key={counter}>{counter}</li>
-            ))}
-          </ul>
-        </dd>
-      </dl>
-    </>
-  )
-}
-
 export type CounterStat = {
   id: string
   label: string
