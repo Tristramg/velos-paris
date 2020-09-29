17824a64e11cd53d40587c4fa37c72a2b8b68537
Tristram Gräbener <tristram@midnighttrains.com>
2020-09-29 15:13:02 +0200

    Add a parameters menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,30 +39,35 @@ export default function AllCounters({ counts, metadata, buildTime }: Props) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Compteurs vélo à Paris</h1>
-      <div className="pb-12 text-sm">
-        <p>Nombre de passages de vélo sur les points de mesure</p>
-        <p>
-          Indiquer la moyenne journalière{' '}
-          <input type="checkbox" onClick={() => setAvg(!avg)} />
-        </p>
-        <p>
-          Source :{' '}
-          <a href="https://parisdata.opendatasoft.com/explore/dataset/comptage-velo-donnees-compteurs/information/">
-            données ouvertes de la ville de Paris
-          </a>{' '}
-          <a href="/compteurs.csv">(données en cache)</a>
-        </p>
-        <p>
-          Données sous licence{' '}
-          <a href="https://opendatacommons.org/licenses/odbl/">ODbL</a>
-        </p>
-        <p>
-          <a href="https://github.com/Tristramg/velos-paris">
-            Code source de la page
-          </a>{' '}
-          sous licence MIT
-        </p>
-        <p>Page générée le {buildTime}</p>
+      <div className="flex">
+        <div className="w-1/2">
+          <h2>Paramètres</h2>
+          <p>
+            Indiquer la moyenne journalière{' '}
+            <input type="checkbox" onClick={() => setAvg(!avg)} />
+          </p>
+        </div>
+        <div className="pb-12 text-sm w-1/2">
+          <p>Nombre de passages de vélo sur les points de mesure</p>
+          <p>
+            Source :{' '}
+            <a href="https://parisdata.opendatasoft.com/explore/dataset/comptage-velo-donnees-compteurs/information/">
+              données ouvertes de la ville de Paris
+            </a>{' '}
+            <a href="/compteurs.csv">(données en cache)</a>
+          </p>
+          <p>
+            Données sous licence{' '}
+            <a href="https://opendatacommons.org/licenses/odbl/">ODbL</a>
+          </p>
+          <p>
+            <a href="https://github.com/Tristramg/velos-paris">
+              Code source de la page
+            </a>{' '}
+            sous licence MIT
+          </p>
+          <p>Page générée le {buildTime}</p>
+        </div>
       </div>
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         <Map counters={stats} highlight={highlight} />
