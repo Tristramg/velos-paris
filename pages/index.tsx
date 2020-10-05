6d29aa38164736f5a7d38594cb1a7ca5f0dad53e
Tristram Gräbener <tristram@midnighttrains.com>
2020-10-05 16:33:12 +0200

    Fix map on small screens

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,7 +110,7 @@ export default function AllCounters({ counts, buildTime }: Props) {
       </div>
 
       <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <div className="row-span-2 col-span-2">
+        <div className="lg:row-span-2 lg:col-span-2 h-64 lg:h-auto">
           <Map counters={stats} highlight={highlight} />
         </div>
         {_.map(stats, (stat, index) => (
