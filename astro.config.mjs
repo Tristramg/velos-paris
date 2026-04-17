bdcbdac23c550e0f4fe90b19b148665cb8b95211
Tristram Gräbener <tristram+git@tristramg.eu>
2026-04-17 14:33:08 +0200

    bump all the things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
   output: 'static',
   base: process.env.BASE_PATH || '/',
   build: {
